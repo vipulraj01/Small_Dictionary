1b7272cd1c680320af642727501a4f884ebc435a
Vipul Raj <38418118+vipulraj01@users.noreply.github.com>
2022-08-12 21:33:26 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+dict = {
+"Introvert":"Shy Person",
+"Extrovert":"An outgoing, socially confident person",
+"Sophicasted":"having, revealing, or involving a great deal of worldly experience and knowledge of fashion and culture"}
+user = input("Enter your Word ")
+print(dict[user])
